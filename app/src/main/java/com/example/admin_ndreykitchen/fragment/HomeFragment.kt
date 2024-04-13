fffb603c57cb602e2ebf43a7e89f3a9594d1c7e4
amiiin94 <amin942004@gmail.com>
2024-04-13 15:01:15 +0700

    chart kelar

--- app/src/main/java/com/example/admin_ndreykitchen/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/admin_ndreykitchen/fragment/HomeFragment.kt
@@ -1,5 +1,7 @@
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,6 +11,7 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.RadioGroup
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
@@ -21,9 +24,21 @@ import com.android.volley.toolbox.Volley
 import com.example.admin_ndreykitchen.R
 import com.example.admin_ndreykitchen.model.ItemModel
 import com.example.admin_ndreykitchen.model.RecordModel
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
+import java.security.KeyStore
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
@@ -41,6 +56,10 @@ class HomeFragment : Fragment() {
     private val itemList = mutableListOf<ItemModel>()
     private lateinit var timeSpanOption: Spinner
 
+    private var profit: Int = 0
+    private var pengeluaranAmount: Int = 0
+    private var pemasukanAmount: Int = 0
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -51,6 +70,7 @@ class HomeFragment : Fragment() {
         // Initialize sharedPreferences
         sharedPreferences = requireActivity().getSharedPreferences("UserData", Context.MODE_PRIVATE)
         return view
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -72,8 +92,9 @@ class HomeFragment : Fragment() {
         selamat_datang.text = "Selamat Datang, $username"
 
         // Setup the spinner
+        // Setup the spinner
         val timeSpanOptions = arrayOf("All Time", "This Month", "This Week", "Today")
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, timeSpanOptions)
+        val adapter = ArrayAdapter(requireContext(), R.layout.spinner_item, timeSpanOptions)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         timeSpanOption.adapter = adapter
 
@@ -97,6 +118,7 @@ class HomeFragment : Fragment() {
 
         // Initially fetch data for all time
         getAllrecords(TimeSpan.ALL_TIME)
+        chartMenu()
     }
 
 
@@ -104,13 +126,13 @@ class HomeFragment : Fragment() {
     private fun displayHomeFragmentData(records: List<RecordModel>, items: List<ItemModel>, timeSpan: TimeSpan) {
         val filteredRecords = filterRecordsByTimeSpan(records, timeSpan)
 
-        val pemasukanAmount = filteredRecords.filter { it.type_record == "pemasukan" }.sumOf { it.amount_record ?: 0 }
-        val pengeluaranAmount = filteredRecords.filter { it.type_record == "pengeluaran" }.sumOf { it.amount_record ?: 0 }
+        pemasukanAmount = filteredRecords.filter { it.type_record == "pemasukan" }.sumOf { it.amount_record ?: 0 }
+        pengeluaranAmount = filteredRecords.filter { it.type_record == "pengeluaran" }.sumOf { it.amount_record ?: 0 }
         val totalTransaksi = filteredRecords.size
 
         val makananTerjual = items.filter { item -> filteredRecords.any { it.id_record == item.record_id } }
             .sumOf { it.quantity }
-        val profit = pemasukanAmount - pengeluaranAmount
+        profit = pemasukanAmount - pengeluaranAmount
 
         if (profit >= 0) {
             tvProfit.text = "+Rp $profit"
@@ -122,6 +144,9 @@ class HomeFragment : Fragment() {
         pengeluaran.text = "Rp$pengeluaranAmount"
         total_transaksi.text = "$totalTransaksi pcs"
         makanan_terjual.text = "$makananTerjual pcs"
+
+        setupBarChart()
+        setupLineChart()
     }
 
     private fun getAllrecords(timeSpan: TimeSpan) {
@@ -250,8 +275,133 @@ class HomeFragment : Fragment() {
         }
     }
 
+    private fun chartMenu() {
+        val chartMenuRadio: RadioGroup = requireView().findViewById(R.id.RadioGroup)
+        val lineChart: LineChart = requireView().findViewById(R.id.lineChart)
+        val barChart: BarChart = requireView().findViewById(R.id.barChart)
+
+        chartMenuRadio.setOnCheckedChangeListener { _, checkedID ->
+            if (checkedID == R.id.rbBarChart){
+                barChart.visibility = View.VISIBLE
+                lineChart.visibility = View.GONE
+            }
+            if (checkedID == R.id.rbLineChart){
+                barChart.visibility = View.GONE
+                lineChart.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun setupBarChart() {
+        //Bar Chart Library Dependency : https://github.com/PhilJay/MPAndroidChart
+        val netAmountRangeDate: TextView = requireView().findViewById(R.id.netAmountRange)
+        netAmountRangeDate.text = "$profit" //show the net amount on textview
+
+        val barChart: BarChart = requireView().findViewById(R.id.barChart)
+
+        val barEntries = arrayListOf<BarEntry>()
+        barEntries.add(BarEntry(1f, pemasukanAmount.toFloat() ))
+        barEntries.add(BarEntry(2f, pengeluaranAmount.toFloat()))
+
+        val xAxisValue= arrayListOf<String>("","Expense", "Income")
+
+        //custom bar chart :
+        barChart.animateXY(500, 500) //create bar chart animation
+        barChart.description.isEnabled = false
+        barChart.setDrawValueAboveBar(true)
+        barChart.setDrawBarShadow(false)
+        barChart.setPinchZoom(false)
+        barChart.isDoubleTapToZoomEnabled = false
+        barChart.setScaleEnabled(false)
+        barChart.setFitBars(true)
+        barChart.legend.isEnabled = false
+
+        barChart.axisRight.isEnabled = false
+        barChart.axisLeft.isEnabled = false
+        barChart.axisLeft.axisMinimum = 0f
 
 
+        val xAxis = barChart.xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.setDrawGridLines(false)
+        xAxis.granularity = 1f
+        xAxis.valueFormatter = com.github.mikephil.charting.formatter.IndexAxisValueFormatter(xAxisValue)
 
+        val barDataSet = BarDataSet(barEntries, "")
+        barDataSet.setColors(
+            resources.getColor(R.color.orange_mainColor),
+            resources.getColor(R.color.orange_pudar)
+        )
+        barDataSet.valueTextColor = Color.BLACK
+        barDataSet.valueTextSize = 15f
+        barDataSet.isHighlightEnabled = false
 
+        //setup bar data
+        val barData = BarData(barDataSet)
+        barData.barWidth = 0.5f
+
+        barChart.data = barData
+    }
+
+    private fun setupLineChart() {
+        // Line Chart Library Dependency: https://github.com/PhilJay/MPAndroidChart
+        val netAmountRangeDate: TextView = requireView().findViewById(R.id.netAmountRange)
+        netAmountRangeDate.text = "$profit" // Show the net amount on textview
+
+        val lineChart: LineChart = requireView().findViewById(R.id.lineChart)
+
+        // Assuming your records contain date strings in the format "dd/MM/yyyy HH:mm"
+        val dateTotalMap = mutableMapOf<String, Int>()
+        var cumulativeSum = 0 // Initialize cumulative sum
+
+// Populate the dateTotalMap with date as key and net amount as value
+        recordList.sortedBy { it.date_record }.forEach { record ->
+            val date = record.date_record?.split(" ")?.get(0) // Extracting date part safely
+            date?.let { // Check if date is not null
+                if (record.type_record == "pemasukan") {
+                    cumulativeSum += record.amount_record ?: 0 // Add pemasukan amount
+                } else {
+                    cumulativeSum -= record.amount_record ?: 0 // Subtract pengeluaran amount
+                }
+                dateTotalMap[it] = cumulativeSum // Store cumulative sum for the date
+            }
+        }
+
+        // Convert map entries to sorted list of entries
+        val lineEntries = dateTotalMap.entries.sortedBy { it.key }
+            .mapIndexed { index, entry -> Entry(index.toFloat(), entry.value.toFloat()) }
+
+        // Custom line chart settings:
+        lineChart.animateXY(500, 500) // Create line chart animation
+        lineChart.description.isEnabled = false
+        lineChart.setDrawGridBackground(false)
+        lineChart.setDrawBorders(false)
+        lineChart.setPinchZoom(false)
+        lineChart.isDoubleTapToZoomEnabled = false
+        lineChart.setScaleEnabled(false)
+        lineChart.legend.isEnabled = false
+
+        val xAxisValue = dateTotalMap.keys.toTypedArray()
+
+        val xAxis = lineChart.xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.setDrawGridLines(false)
+        xAxis.granularity = 1f
+        xAxis.valueFormatter = IndexAxisValueFormatter(xAxisValue)
+
+        val yAxisRight = lineChart.axisRight
+        yAxisRight.isEnabled = false
+
+        val lineDataSet = LineDataSet(lineEntries, "")
+        lineDataSet.color = resources.getColor(R.color.orange_mainColor)
+        lineDataSet.setCircleColor(resources.getColor(R.color.orange_mainColor))
+        lineDataSet.setDrawValues(true)
+        lineDataSet.valueTextColor = Color.BLACK
+        lineDataSet.valueTextSize = 15f
+        lineDataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+
+        val lineData = LineData(lineDataSet)
+
+        lineChart.data = lineData
+    }
 }
